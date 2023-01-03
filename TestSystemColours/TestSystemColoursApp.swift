97fe813e68d19e6b4c2fbd9a7ca573114cf8e7a8
JulesMoorhouse <361024+JulesMoorhouse@users.noreply.github.com>
2023-01-03 19:19:54 +0000

    Initial Commit

---
+++ TestSystemColours/TestSystemColoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSystemColoursApp.swift
+//  TestSystemColours
+//
+//  Created by Jules Moorhouse on 03/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestSystemColoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
