53671f97035469f4da5e0c907b0acb9d5d5d6cdd
JulesMoorhouse <361024+JulesMoorhouse@users.noreply.github.com>
2023-01-03 19:42:57 +0000

    chore: added colours.

--- TestSystemColours/ContentView.swift
+++ TestSystemColours/ContentView.swift
@@ -7,14 +7,44 @@
 
 import SwiftUI
 
+extension Color {
+    static let systemBackground = Color(UIColor.systemBackground)
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+    static let semiDisabledBackground = Color.secondary.opacity(0.2)
+}
+
 struct ContentView: View {
+    let w = 270.0
+    let h = 40.0
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack { Text("Primary").foregroundColor(.white) }
+                .frame(width: w, height: h)
+                .background(Color.primary)
+
+            VStack { Text("Secondary") }
+                .frame(width: w, height: h)
+                .background(Color.secondary)
+
+            VStack { Text("systemBackground") }
+                .frame(width: w, height: h)
+                .background(Color.systemBackground)
+
+            VStack { Text("systemGroupedBackground") }
+                .frame(width: w, height: h)
+                .background(Color.systemGroupedBackground)
+
+            VStack { Text("secondarySystemGroupedBackground") }
+                .frame(width: w, height: h)
+                .background(Color.secondarySystemGroupedBackground)
+
+            VStack { Text("semiDisabledBackground") }
+                .frame(width: w, height: h)
+                .background(Color.semiDisabledBackground)
         }
+        .font(.caption)
         .padding()
     }
 }
